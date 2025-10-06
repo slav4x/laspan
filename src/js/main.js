41770323c96da8188bd38c40050c5cc29c51c15a
Slava Khripushin <slava.cpa@yandex.ru>
2025-10-06 15:44:47 +0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  Fancybox.bind('[data-fancybox]', {
+    dragToClose: false,
+    autoFocus: false,
+    placeFocusBack: false,
+    Thumbs: false,
+    Images: {
+      zoom: false,
+    },
+  });
+
+  const maskOptions = {
+    mask: '+7 (000) 000-00-00',
+    onFocus() {
+      if (this.value === '') this.value = '+7 ';
+    },
+    onBlur() {
+      if (this.value === '+7 ') this.value = '';
+    },
+  };
+
+  document.querySelectorAll('.masked').forEach((item) => new IMask(item, maskOptions));
+});
